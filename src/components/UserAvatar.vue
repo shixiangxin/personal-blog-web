81168397cc3cddfd06a3806762f0d603c2f4584e
ShiXiangXin <shixiangxin7@gmail.com>
2026-07-23 21:15:36 +0800

    feat:新增Avatar组件

---
+++ src/components/UserAvatar.vue
@@ -0,0 +1,36 @@
+<template>
+  <div
+    class="avatar-container"
+    :style="{
+      width: size + 'px',
+      height: size + 'px',
+    }"
+  >
+    <img :src="url" alt="" />
+  </div>
+</template>
+<script setup lang="ts">
+defineProps({
+  url: {
+    type: String,
+    required: true,
+  },
+  size: {
+    type: Number,
+    default: 100,
+  },
+})
+</script>
+<style lang="scss" scoped>
+@use '@/styles/mixin';
+.avatar-container {
+  margin: 0 auto;
+}
+img {
+  display: block;
+  width: 100%;
+  height: 100%;
+  border-radius: 50%;
+  overflow: hidden;
+}
+</style>
