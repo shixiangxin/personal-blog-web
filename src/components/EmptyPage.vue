248c6d672a608ef1db4b4a7887954573d4e5d78b
ShiXiangXin <shixiangxin7@gmail.com>
2026-07-24 20:48:32 +0800

    feat:新增EmptyPage组件

---
+++ src/components/EmptyPage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="empty-page-container">
+    <AppIcon type="empty"></AppIcon>
+    <p>{{ text }}</p>
+  </div>
+</template>
+<script setup lang="ts">
+import AppIcon from '@/components/AppIcon.vue'
+interface Props {
+  text?: string
+}
+withDefaults(defineProps<Props>(), {
+  text: '暂无内容',
+})
+</script>
+<style scoped lang="scss">
+@use '/src/styles/mixin';
+@use '/src/styles/var';
+.empty-page-container {
+  @include mixin.self-center(absolute);
+  color: var.$gray;
+  text-align: center;
+  .icon-container {
+    font-size: 100px;
+  }
+  p {
+    font-size: 20px;
+    margin: 0;
+  }
+}
+</style>
