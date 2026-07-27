a67d062dd2d8976d05cbbec7d3a5a9f884bd393d
ShiXiangXin <shixiangxin7@gmail.com>
2026-07-27 22:59:16 +0800

    feat:新增ImageLoader组件

---
+++ src/components/ImageLoader.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="image-loader-container">
+    <img v-if="everythingDone" class="placeholder" :src="placeholder" alt="" />
+    <img
+      @load="handleLoad"
+      :src="url"
+      alt=""
+      :style="{
+        opacity: +originLoaded,
+        transition: duration + 'ms',
+      }"
+    />
+  </div>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue'
+const originLoaded = ref(false) //原图是否加载完成
+const everythingDone = ref(false) //是否全部完成
+interface Props {
+  url: string
+  placeholder: string
+  duration?: number
+}
+interface Emits {
+  (e: 'load'): void
+}
+const props = withDefaults(defineProps<Props>(), {
+  duration: 500,
+})
+const emit = defineEmits<Emits>()
+
+function handleLoad() {
+  originLoaded.value = true
+  setTimeout(() => {
+    everythingDone.value = true
+    emit('load')
+  }, props.duration)
+}
+</script>
+<style lang="scss" scoped>
+@use '/src/styles/mixin.scss';
+.image-loader-container {
+  width: 100%;
+  height: 100%;
+  position: relative;
+  img {
+    @include mixin.self-center();
+    object-fit: cover;
+  }
+  .placeholder {
+    filter: blur(2vw);
+  }
+}
+</style>
